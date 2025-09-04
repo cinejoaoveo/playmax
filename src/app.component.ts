ea0581449ba1ab715f06cfb10f52db8b51357a07
cinejoaoveo <cinejoaoveo@gmail.com>
2025-09-04 01:33:35 -0300

    Add files via upload

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component'; // <-- ADICIONE ESTA LINHA
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent], // <-- ADICIONE O FooterComponent AQUI
 })
 export class AppComponent {}
